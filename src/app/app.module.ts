1b66939373df9f4de13cd844b66d80df838319da
Subhan-Hafiz <subhanhafiz12@gmail.com>
2019-10-19 19:34:23 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
+import { NewProductFormComponent } from './new-product-form/new-product-form.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './order/order.component';
+import { SuccessComponent } from './success/success.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import {FormsModule} from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    OrderComponent,
+    SuccessComponent,
+    LoginComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    NewProductFormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'products', component: ProductsComponent},
+      {path: 'cart', component: CartComponent},
+      {path: 'order-review', component: OrderComponent},
+      {path: 'success', component: SuccessComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'new', component: NewProductFormComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
